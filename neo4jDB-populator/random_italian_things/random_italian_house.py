77d39c4eebb8708564ec49b24b36d436b2a57635
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2021-11-05 23:28:42 +0100

    added _load_data method to improve readability of __init__ in random_italian_house.py

--- neo4jDB-populator/random_italian_things/random_italian_house.py
+++ neo4jDB-populator/random_italian_things/random_italian_house.py
@@ -16,22 +16,26 @@ class RandomItalianHouse:
 
         if RandomItalianHouse.addresses is None:
             if RandomItalianHouse.addresses is None:
-                # read 'datasets_to_read' that contains as keys the name of the city to read and as values the name
-                # of its file.
-                datasets_to_read = pd.read_csv(
-                    "{}/datasets/datasets_to_read.txt".format(
-                        os.path.dirname(os.path.abspath(__file__)))
-                )
-                # create a dictionary containing for each city all its possible addresses
-                RandomItalianHouse.addresses = {}
-                for index, row in datasets_to_read.iterrows():
-                    self._read_city(row['value'], row['key'])
+                self._load_data()
 
         address_data = RandomItalianHouse.addresses[city].sample(n=1)
 
         self._data = {
             **address_data.reset_index(drop=True).iloc[0].to_dict()
         }
+
+    @staticmethod
+    def _load_data():
+        # read 'datasets_to_read' that contains as keys the name of the city to read and as values the name
+        # of its file.
+        datasets_to_read = pd.read_csv(
+            "{}/datasets/datasets_to_read.txt".format(
+                os.path.dirname(os.path.abspath(__file__)))
+        )
+        # create a dictionary containing for each city all its possible addresses
+        RandomItalianHouse.addresses = {}
+        for index, row in datasets_to_read.iterrows():
+            RandomItalianHouse._read_city(row['value'], row['key'])
 
     @staticmethod
     def _read_city(file_name: str, name_key: str):
