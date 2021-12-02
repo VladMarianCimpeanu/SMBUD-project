08770c7ccba3c2ee41fdf77d6f9da43c6f3ffb6d
pasqualedazzeo <48350190+pasqualedazzeo@users.noreply.github.com>
2021-12-02 16:36:23 +0100

    added creation of places

--- MongoDB_assignment/main.py
+++ MongoDB_assignment/main.py
@@ -1,11 +1,16 @@
-from random_italian_things import RandomItalianPerson, RandomItalianHouse, random_amenity
-from random_italian_things.utils import date_generator as dg, date_facilities
+#from random_italian_things import RandomItalianPerson, RandomItalianHouse, random_amenity
+#from random_italian_things.utils import date_generator as dg, date_facilities
 from pymongo import MongoClient
 from pprint import pprint
-
+import pandas as pd
 
 class MongoPopulate:
+    
+    client = None
+    db = None
+    
     def __init__(self, connection_string):
+        global client, db
         client = MongoClient(connection_string)
         db = client.SMBUD
 
@@ -18,9 +23,28 @@ class MongoPopulate:
             "surname": random_italian_person.surname,
             "tax_code": random_italian_person.tax_code,
         }
+    
+    @staticmethod
+    def create_places():
+        collection = db.places
+        places_df = pd.read_csv('datasets/locations.csv')
+        places = []
+        for index,row in places_df.iterrows():
+            place = {
+                "building_name" : row.building_name,
+                "type" : row.type,
+                "region" : row.region
+            }
+            places.append(place)
+        result = collection.insert_many(places)
+        print(result)
+            
+            
 
 
 if __name__ == "__main__":
     with open("connection_string.txt", "r") as connection_string_reader:
         connection_string = connection_string_reader.readline().split()[0]
         mongo_populate = MongoPopulate(connection_string)
+        MongoPopulate.create_places()
+        
